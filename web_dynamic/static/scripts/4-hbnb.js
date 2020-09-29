d37682f8049a898396e5b8240e156c1bb9613943
modernfuthark <timothys1738@gmail.com>
2020-09-28 20:06:12 -0400

    Added javascript file for task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,48 @@
+const $ = window.$;
+const amens = {};
+
+$(document).ready(function () {
+  // const amens = {}; Moves out of function so it can be checked in the button function
+  $('input[type=checkbox]').change(
+    function () {
+      if (this.checked) {
+        // alert($(this).attr('data-id'));
+        amens[($(this).attr('data-name'))] = $(this).attr('data-attr');
+      } else {
+        delete amens[$(this).attr('data-name')];
+      }
+      // this just prints the list, debug stuff
+      // alert(Object.keys(amens));
+
+      $('.amenities h4').text(Object.keys(amens));
+    });
+
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function () {
+      $('#api_status').addClass('available');
+    },
+    error: function () {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
+
+$('button').click(function () {
+  const amenities_id = Object.keys(amens);
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: JSON.stringify({amenities: amenities_id}),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        const place = data[i];
+        const _html = `<article><div class="title_box"> <h2> ${place.name} </h2> <div class="price_by_night">$${place.price_by_night}</div></div><div class="information"><div class="max_guest"><div class="guest_image"></div>${place.max_guest} Guests </div><div class="number_rooms"><div class="bed_image"></div>${place.number_rooms} Bedrooms</div><div class="number_bathrooms"><div class="bath_image"></div>${place.number_bathrooms} Bathrooms</div></div><div class="description">${place.description}</div></article>`;
+        $('section.places').append('' + _html);
+      }
+    }
+  });
+});
